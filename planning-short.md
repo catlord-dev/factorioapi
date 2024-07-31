e09b856a262ebb0612ad02a37136f6642a93bc86
catlord-dev <catlord03012012151804@gmail.com>
2024-07-30 23:28:07 -0400

    some clearer planning stuff and things

---
+++ planning-short.md
@@ -0,0 +1,29 @@
+# FactorioAPI
+## API
+### Internal
+Download - ❌  
+Matchmaking - ✅ 
+Mod Portal - ❌  
+Web Auth - ✅
+### Public
+Mod Details - ❌  
+Mod Images - ❌  
+Mod Publish - ❌  
+Mod Upload - ❌
+## Data
+Utils - 🔧
+### Files
+Achievements - ❌  
+Mod Settings - ✅
+### IO
+Read - ✅
+Write - ✅
+### Strings
+Blueprints - ❌  
+Map Exchange - ❌
+  
+# Key
+✅ - finished  
+🔧 - Work in Progress  
+⚙️ - in progress  
+❌ - todo / not working on it rn
--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='factorioapi',
-    version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        # List your library's dependencies here
-    ],
-    # entry_points={
-    #     'console_scripts': [
-    #         'your-command=your_library.module:function',
-    #     ],
-    # },
-    author='Cat Lord',
-    author_email='catlord03012012151804@gmail.com',
-    description='A Python library to interact with the Factorio API and related files',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/catlord-dev/factorioapi',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: GNU AGPL v3',
-        'Operating System :: OS Independent',
-    ],
 )
